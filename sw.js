3b7cde2c9baa000af88d77671ef6181922e5d73a
paolocastagnola-jpg <paolo.castagnola@gmail.com>
2025-12-10 11:30:21 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,41 @@
-const CACHE = "pizzeria-v1";
-const assets = [
+const CACHE_NAME = "pizzeria-v2";  // <- nuovo nome cache
+const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
   "./manifest.json",
   "./img/logo-192.png",
-  "./img/logo-512.png"
+  "./img/logo-512.png",
+  "./img/placeholder.jpg"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+// Cancella le vecchie cache quando arriva questa versione
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
   );
 });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
